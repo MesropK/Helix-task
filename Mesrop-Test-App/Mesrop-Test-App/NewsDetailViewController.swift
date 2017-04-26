003b3606c7488773370e8f0356f4a28a45ab6b68
Mesrop Kareyan <Mesrop@Mesbook.local>
2017-04-27 01:51:36 +0400

    Scroll view fix

--- Mesrop-Test-App/Mesrop-Test-App/NewsDetailViewController.swift
+++ Mesrop-Test-App/Mesrop-Test-App/NewsDetailViewController.swift
@@ -18,9 +18,25 @@ class NewsDetailViewController: UIViewController {
     @IBOutlet weak var titleLabel:              UILabel!
     @IBOutlet weak var categoryLabel:           UILabel!
     @IBOutlet weak var galleryItemsView:        UIView!
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var titleView: UIView!
 
+
+    @IBOutlet weak var topViewTopConstraint: NSLayoutConstraint!
+    
+    var lastTopOffsetY: CGFloat = 0
+    let navBarHeight: CGFloat = 60
+
+    
     func configureView() {
         // Update the user interface for the news item.
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        let offset = CGPoint(x: 0, y: navBarHeight)
+        newsWebView.scrollView.contentInset = insets
+        newsWebView.scrollView.scrollIndicatorInsets = insets
+        newsWebView.scrollView.setContentOffset(offset, animated: false)
+        newsWebView.scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         if let news = self.newsItem {
             //news title ui
             titleLabel.text = news.title
@@ -63,7 +79,7 @@ class NewsDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newsWebView.scrollView.delegate = self;
         configureView()
     }
 
@@ -112,5 +128,49 @@ class NewsDetailViewController: UIViewController {
         present(browser, animated: true, completion: {})
     }
 
+}
+
+//MARK: ScrollView delegate
+extension NewsDetailViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard scrollView === newsWebView.scrollView else {
+            return
+        }
+        guard let _ = self.navigationController?.navigationBar else {
+            return
+        }
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        if scrollView === newsWebView.scrollView {
+            self.lastTopOffsetY = scrollView.contentOffset.y;
+        }
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView === newsWebView.scrollView {
+        }
+    }
+    
+    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        if scrollView === newsWebView.scrollView {
+            let hide = newsWebView.scrollView.contentOffset.y > lastTopOffsetY
+           setTopView(hidden: hide)
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if scrollView === newsWebView.scrollView {
+        }
+    }
+    
+    func setTopView(hidden isHidden: Bool) {
+        topViewTopConstraint.constant =  isHidden ? -360 : 0
+        UIView.animate(withDuration: 0.4) { 
+            self.view.layoutIfNeeded()
+        }
+    }
+    
 }
 
